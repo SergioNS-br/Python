ed66080fd926a42f53305ba3179754e8e710b6b0
SergioNS-br <inf.souza@gmail.com>
2024-05-01 18:58:09 -0300

    Atualização do arq operadores.py

---
+++ programacao_python_zero_avancado/operadores.py
@@ -0,0 +1,54 @@
+#  == equal
+#  != not equal
+#  > greater than
+#  < less than
+#  >= greater than or equal to
+#  <= less than or equal to
+
+
+x = 10  
+x += 1
+print (f"Soma: {x}")
+
+x = 10 
+x -= 3
+print (f"Subtração: {x}")
+
+x = 10
+x /= 5
+print (f"Divisão: {x}")
+
+x = 10
+x %= 4
+print (f"Divisão Resto: {x}")
+
+
+x = 10
+x *= 2.7
+print (f"Multiplicação: {x}")
+
+
+'''
+valor1=10
+valor2=10
+valor3=52
+valor4=40
+
+print (f"Teste01: {valor1==valor2}")
+print (f"Teste02: {valor1==valor3}")
+print ("")
+print (f"Teste03: {valor1!=valor3}")
+print (f"Teste04: {valor1!=valor2}")
+print ("")
+print (f"Teste05: {valor3>valor4}")
+print (f"Teste06: {valor4>valor3}")
+print ("")
+print (f"Teste07: {valor4<valor3}")
+print (f"Teste08: {valor3<valor4}")
+print ("")
+print (f"Teste09: {valor1>=valor2}")
+print (f"Teste10: {valor1>=valor3}")
+print ("")
+print (f"Teste11: {valor1<=valor2}")
+print (f"Teste12: {valor3<=valor4}")
+'''
