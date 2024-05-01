41f7fd00fc3ebdb97b14d6f04bb43ceedbc5908d
SergioNS-br <inf.souza@gmail.com>
2024-05-01 16:27:58 -0300

    inclusao do pgm basico.py

---
+++ programacao_python_zero_avancado/basico.py
@@ -0,0 +1,68 @@
+'''
+INDICE
+    Print
+    Comentários
+    Variáveis
+    Input
+'''
+
+'''
+#Print
+print ("\n")
+print ("Teste print de Python")
+print ("Segundo teste print de Python")
+
+print ("")
+
+curso = "Python"
+horas = 40
+print("O curso de " + curso + " tem " + str(horas) + " horas no total")
+
+print ("")
+
+curso2 = "AWS"
+horas2 = str(40)
+print("O curso de " + curso2 + " tem mais que " + horas2 + " horas")
+print ("----------------------------------")
+
+#Comentários
+print ("\n") 
+print ("Comentário de linha começa com #")
+print ("Comentário em bloco começa e termina com 3 aspas simples "'''" texto "'''" ")
+print ("----------------------------------")
+
+#Variáveis
+print ("\n") 
+x = 2 
+print (x)
+
+y = "Olá"
+print (y)
+
+z = 2.3
+print (z)
+
+print ("")
+
+x = str("3")
+y = int (2)
+z = float (5)
+print(x)
+print(y)
+print(z)
+print ("----------------------------------")
+
+'''
+
+#Input
+print ("\n")
+valor1 = input("Digite um número  ")
+valor2 = input("Digite mais um número ")
+resultado = int(valor1) + int(valor2)
+# saber o tipo da variável
+print ("valor1: " + str(type(valor1)))
+print ("valor2: " + str(type(valor2)))
+print ("resultado: " + str(type(resultado)))
+print ("A soma dos 2 números é: " + str(resultado) )
+print ("---------------------------------")
+
